e5eebfef2498ab561de83df8404cf602bdc7e3dc
Adithyan Anil <adithyananil2002@gmail.com>
2023-10-14 13:10:49 +0530

    added redux

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment,reset } from "./redux/counterSlice";
 
 export default function Counter() {
+
+const dispatch=useDispatch()
+
+const count = useSelector((state):(state)=>state.counter.count)
+
+
   return (
     <div className="counter-card">
-      <div className="result-field">100</div>
+      <div className="result-field">{count}</div>
       <div className="btn-collection">
-        <button className="btn decrement ">Decrement</button>
-        <button className="btn reset">reset</button>{" "}
-        <button className="btn increment ">Increment</button>{" "}
+        <button onClick={()=>dispatch(decrement())} className="btn decrement ">Decrement</button>
+        <button onClick={()=>dispatch(reset())} className="btn reset">reset</button>{" "}
+        <button onClick={()=>dispatch(increment())} className="btn increment ">Increment</button>{" "}
       </div>
     </div>
   );
